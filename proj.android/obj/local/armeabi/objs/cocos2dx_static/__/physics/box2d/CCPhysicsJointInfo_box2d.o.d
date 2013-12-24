063cff4cf9d954a9888ed0c224a74070e051c905
komashen <sxxdjj@gmail.com>
2013-12-24 13:55:45 +0800

    deleted .a

--- proj.android/obj/local/armeabi/objs/cocos2dx_static/__/physics/box2d/CCPhysicsJointInfo_box2d.o.d
+++
@@ -1,8 +0,0 @@
-obj/local/armeabi/objs/cocos2dx_static/__/physics/box2d/CCPhysicsJointInfo_box2d.o: \
- /Volumes/Work/cocos2d-x/cocos2d-x-3.0alpha1/projects/Fighter/proj.android/../../../cocos/2d/../physics/box2d/CCPhysicsJointInfo_box2d.cpp \
- /Volumes/Work/cocos2d-x/cocos2d-x-3.0alpha1/projects/Fighter/proj.android/../../../cocos/2d/../physics/box2d/CCPhysicsJointInfo_box2d.h \
- /Volumes/Work/cocos2d-x/cocos2d-x-3.0alpha1/projects/Fighter/proj.android/../../../cocos/2d/../physics/box2d/../CCPhysicsSetting.h
-
-/Volumes/Work/cocos2d-x/cocos2d-x-3.0alpha1/projects/Fighter/proj.android/../../../cocos/2d/../physics/box2d/CCPhysicsJointInfo_box2d.h:
-
-/Volumes/Work/cocos2d-x/cocos2d-x-3.0alpha1/projects/Fighter/proj.android/../../../cocos/2d/../physics/box2d/../CCPhysicsSetting.h:
