59ca36223558403e9c7c0fa4d281cceb649671e0
komashen <sxxdjj@gmail.com>
2013-12-24 13:04:07 +0800

    add GameStartScen.h

---
+++ Classes/Scene/GameStartScene.h
@@ -0,0 +1,14 @@
+//
+//  GameStartScene.h
+//  Fighter
+//
+//  Created by shen xinxing on 12/24/13.
+//
+//
+
+#ifndef __Fighter__GameStartScene__
+#define __Fighter__GameStartScene__
+
+#include <iostream>
+
+#endif /* defined(__Fighter__GameStartScene__) */
