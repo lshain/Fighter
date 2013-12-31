d479a3b982ff031b8bb3f009e96d55539be82b48
komashen <sxxdjj@gmail.com>
2013-12-31 11:42:25 +0800

    modified GameMacro.h, GameStartScene.cpp, GameStartScene.h, GameStartSceneLoader.h

---
+++ Classes/Scene/GameStartSceneLoader.h
@@ -0,0 +1,14 @@
+//
+//  GameStartSceneLoader.h
+//  Fighter
+//
+//  Created by shen xinxing on 12/31/13.
+//
+//
+
+#ifndef Fighter_GameStartSceneLoader_h
+#define Fighter_GameStartSceneLoader_h
+
+
+
+#endif
